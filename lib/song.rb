0219ba1b0cfee1dbae1f98d5bb3cd6ead58fbb33
Amber Taranto <ambzzzz@gmail.com>
2019-08-08 03:21:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   extend Memorable
   extend Findable
   include Paramable
-  
+
   attr_accessor :name
   attr_reader :artist
 
@@ -34,7 +34,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
